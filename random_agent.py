2efba6d9c28d7ae441209ecb1b88f482e3023daf
Piotr Gardocki <pgardocki@interia.pl>
2023-12-04 01:09:02 +0100

    Add random play agent

---
+++ random_agent.py
@@ -0,0 +1,14 @@
+from env import SpaceInvaders2Env
+
+
+env = SpaceInvaders2Env(render_mode="human")
+observation, info = env.reset()
+
+for _ in range(1000):
+    action = env.action_space.sample()
+    observation, reward, terminated, truncated, info = env.step(action)
+
+    if terminated or truncated:
+        observation, info = env.reset()
+
+env.close()
